b212afd5f7ff22a6f14d328281dd1cddeb2c9f0f
aniseferreira <aniseferreira@users.noreply.github.com>
2020-07-05 20:52:18 +0000

    Deploying to gh-pages from  @ 861f7f1fa55a1a8b14de855c8608138a0e6ab642 🚀

---
+++ precache-manifest.cbcfee6e9fbe46542bd83df6f60badeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4b6bc67e74b71a65fa4673ca221e9f",
+    "url": "/treebank-template/index.html"
+  },
+  {
+    "revision": "2dcbad217f7877ce0091",
+    "url": "/treebank-template/static/css/2.354f031a.chunk.css"
+  },
+  {
+    "revision": "e049df9eab49c4dd7012",
+    "url": "/treebank-template/static/css/main.bd98509e.chunk.css"
+  },
+  {
+    "revision": "2dcbad217f7877ce0091",
+    "url": "/treebank-template/static/js/2.1716a128.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/treebank-template/static/js/2.1716a128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e049df9eab49c4dd7012",
+    "url": "/treebank-template/static/js/main.637c5dbe.chunk.js"
+  },
+  {
+    "revision": "fc82e1a45c5889b39b98",
+    "url": "/treebank-template/static/js/runtime-main.94c54c7b.js"
+  }
+]);
