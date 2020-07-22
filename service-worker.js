a732185b8399f634de17c8b8d87d7f3c12327ef2
aniseferreira <aniseferreira@users.noreply.github.com>
2020-07-22 14:04:21 +0000

    Deploying to gh-pages from  @ 3514faff91e842fc3730ee7c5882674e57f6e02d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/treebank-template/precache-manifest.6520ca8feeff6659fb603fccce6ffb08.js"
+  "/treebank-template/precache-manifest.fb7b7bffca45a67e6772748464e40900.js"
 );
 
 self.addEventListener('message', (event) => {
